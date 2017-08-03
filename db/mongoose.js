d72044fc227fb50bd23b92985560c8784a26ce30
Sameep Singhania <sameepsi@gmail.com>
2017-08-04 01:20:34 +0530

    start script

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 
-var db = process.env.MONGODB_URL || "mongodb://localhost:27017/YelpCamp"
+var db = process.env.MONGODB_URI || "mongodb://localhost:27017/YelpCamp"
 mongoose.connect(db);
 
 module.exports={
